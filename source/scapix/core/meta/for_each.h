fe7ed9c40eaf62c54c31706a4d2bd26627d8e512
Boris Rasin <boris@pointx.org>
2026-05-08 00:32:21 +0300

    refactor: change meta::for_each interface

--- source/scapix/core/meta/for_each.h
+++ source/scapix/core/meta/for_each.h
@@ -8,7 +8,6 @@
 #define SCAPIX_CORE_META_FOR_EACH_H
 
 #include <utility>
-#include <initializer_list>
 
 namespace scapix::meta {
 namespace detail {
@@ -20,18 +19,28 @@ template <template <class...> class L, class ...T>
 struct for_each_impl<L<T...>>
 {
 	template <class F>
-	static constexpr F call(F&& f)
+	static constexpr void call(F&& f)
 	{
-		return (void)std::initializer_list<int>{((void)f(T()), 0)...}, f;
+		(std::forward<F>(f).template operator()<T>(), ...);
+	}
+};
+
+template <template <auto...> class L, auto ...V>
+struct for_each_impl<L<V...>>
+{
+	template <class F>
+	static constexpr void call(F&& f)
+	{
+		(std::forward<F>(f).template operator()<V>(), ...);
 	}
 };
 
 } // namespace detail
 
 template <class L, class F>
-constexpr F for_each(F&& f)
+constexpr void for_each(F&& f)
 {
-	return detail::for_each_impl<L>::call(std::forward<F>(f));
+	detail::for_each_impl<L>::call(std::forward<F>(f));
 }
 
 } // namespace scapix::meta
